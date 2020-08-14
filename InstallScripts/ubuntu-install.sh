6cbdefbdf46d16d064117bae22625edbd23c2707
Tyler <tjflaagan@users.noreply.github.com>
2020-08-13 21:55:14 -0500

    Update ubuntu-install.sh

--- InstallScripts/ubuntu-install.sh
+++ InstallScripts/ubuntu-install.sh
@@ -16,7 +16,7 @@ apt update -y --fix-missing
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt update -y 
-apt install sublime-text
+apt install -y sublime-text
 
 # Install terminator
 apt install -y terminator
