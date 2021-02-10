54b83814f8f7f73e3bb8396096e506ce611aee29
Tyler <tjflaagan@users.noreply.github.com>
2021-02-10 14:09:50 -0600

    Force sublime install -y TF

--- InstallScripts/kali-install.sh
+++ InstallScripts/kali-install.sh
@@ -47,7 +47,7 @@ make
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt update -y 
-apt install sublime-text
+apt install -y sublime-text
 
 # Install Atom
 wget -O /tmp/atom-amd64.deb https://github.com/atom/atom/releases/download/v1.49.0/atom-amd64.deb                                                                                                                                          
